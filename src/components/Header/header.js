46f3b1a0dfb95cd2fbe3a33dd68eea25570fdd4f
Gregory Le Terte <gregory.leterte@gmail.com>
2021-06-03 06:54:10 +0200

    Update Header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,32 +1,38 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-
+import { StaticImage } from "gatsby-plugin-image"
+import "../base.css"
+import "./header.css"
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `midnightblue`,
-      marginBottom: `1 rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="Header">
+    <Link to="/">
+      <StaticImage
+        src="../../images/logo-icon.png"
+        width={40}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Logo Icone Ennovsys"
+        className="logo-icone"
+      />
+      <StaticImage
+        src="../../images/Logo_Texte_EnnovSys.png"
+        width={120}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Logo Ennovsys"
+        className="logo"
+      />
+    </Link>
+
+    <div className="Menu">
+      <ul>
+        <li>Projet au Forfait</li>
+        <li>Assistance Technique</li>
+        <li>Maintenance Applicative</li>
+        <li>UX/UIX</li>
+        <li>Contact</li>
+      </ul>
     </div>
   </header>
 )

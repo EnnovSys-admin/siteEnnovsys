59269c361f25f079208554f811b3f51158b0fc2c
Gregory Le Terte <gregory.leterte@gmail.com>
2021-06-07 07:07:09 +0200

    Init and insert link Foorter

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,21 +1,57 @@
 import * as React from "react"
 //import PropTypes from "prop-types"
 //import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import "../base.css"
 import "./footer.css"
 
 const Footer = () => (
   <footer>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        fontSize: "1rem",
-      }}
-    >
-      Tous droits réservés. 2 Bis Rue Robert Le Ricolais - 44300 NANTES
-      <p>© ENNOVSYS 2014 - {new Date().getFullYear()}</p>
+    <div className="conteneur">
+      Bloc footer En cours de développement
+      <div className="footer_align">
+        <div className="logo">
+          <StaticImage
+            src="../../images/logo-icon.png"
+            width={110}
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Logo Icone Ennovsys"
+            className="logo-icone"
+          />
+        </div>
+        <div className="List 1">
+          <ul>
+            <div>Offres de Services</div>
+            <li>Projet au forfait </li>
+            <li>Assistance Technique</li>
+            <li>Maintenance Applicative</li>
+            <li>UX/UIX</li>
+          </ul>
+        </div>
+        <div className="List 2">
+          <ul>
+            <div>Les pôles d'Ennovsys</div>
+            <li>Pôle PcSoft</li>
+            <li>Pôle digital</li>
+            <li>Technologie</li>
+          </ul>
+        </div>
+
+        <div className="List 3">
+          <ul>
+            <div>Qui Sommes nous ?</div>
+            <li>Ennovsys la société</li>
+            <li>Nos Partenaires</li>
+            <li>Jobs</li>
+            <li>Rejoignez-nous sur : Linkedin </li>
+          </ul>
+        </div>
+      </div>
+      <div className="society">
+        Tous droits réservés. 2 Bis Rue Robert Le Ricolais - 44300 NANTES
+        <p> © ENNOVSYS 2014 - {new Date().getFullYear()}</p>
+      </div>
     </div>
   </footer>
 )

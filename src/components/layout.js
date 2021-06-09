be3e8ba6f86248d5c9266cce5f2008b539f7a9bf
Gregory Le Terte <gregory.leterte@gmail.com>
2021-06-09 07:06:20 +0200

    Insert block Hook text

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header/header"
 import Footer from "./Footer/footer"
+import BlockHook from "./BlocHook/blocHook"
 import "./base.css"
 
 const Layout = ({ children }) => {
@@ -27,15 +28,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+
+      <main>
+        <BlockHook />
+        {children}
+      </main>
+
       <Footer />
     </>
   )
